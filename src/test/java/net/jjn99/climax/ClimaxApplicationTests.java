a87fc858832f4aafeb64ba3e6ca51243782dbe16
=JeanJacques <jjn99@outlook.fr>
2024-03-25 22:20:25 +0000

    Initial commit

---
+++ src/test/java/net/jjn99/climax/ClimaxApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jjn99.climax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
